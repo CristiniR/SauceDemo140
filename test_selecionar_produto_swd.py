fd6f65176e4b1c019ba3be8222b5bbc23dd23d82
CristiniR <cristini.rafaela@hotmail.com>
2025-03-07 10:28:44 -0300

    Conclusão do script

--- test_selecionar_produto_swd.py
+++ test_selecionar_produto_swd.py
@@ -13,7 +13,7 @@ class Teste_Produtos():
 
 #2.2 Funções e Métodos-------------------------------------------------------------------------------------------------------
 
-    def setup_method(self, method):                   # método de inicialização dos testes
+    def setup_method(self, method):                    # método de inicialização dos testes
         self.driver = webdriver.Chrome()               # instancia o objeto do Selenium webdriver como Chrome
         self.driver.implicitly_wait(10)                # define o tempo de espera padrão em 10 segundos 
 
@@ -22,9 +22,18 @@ class Teste_Produtos():
 
     def test_selecionar_produto(self):                 # método de teste
         self.driver.get(self.url)
-        self.driver.find_element(By.ID,"user-name").send_keys("standard_user")           # escreve o login 
-        self.driver.find_element(By.ID,"password").send_keys("secret_sauce")             # escreve a senha 
-        self.driver.find_element(By.ID,"login-button").click()                             # clicar botão login 
-        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"    # identificar a palavra PRoducts 
+        self.driver.find_element(By.ID,"user-name").send_keys("standard_user")                     # escreve o login 
+        self.driver.find_element(By.ID,"password").send_keys("secret_sauce")                       # escreve a senha 
+        self.driver.find_element(By.ID,"login-button").click()                                     # clicar botão login 
+        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Products"              # identificar a palavra PRoducts 
         assert self.driver.find_element(By.ID, "item_4_title_link").text == "Sauce Labs Backpack"
         assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item:nth-child(1) .inventory_item_price").text == "$29.99"
+        self.driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack").click()  
+        assert self.driver.find_element(By.CSS_SELECTOR, ".shopping_cart_badge").text == "1"
+        self.driver.find_element(By.ID,"shopping_cart_container").click() 
+        assert self.driver.find_element(By.CSS_SELECTOR, ".title").text == "Your Cart"
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item_name").text == "Sauce Labs Backpack"
+        assert self.driver.find_element(By.CSS_SELECTOR, ".inventory_item_price").text == "$29.99"
+        self.driver.find_element(By.ID,"remove-sauce-labs-backpack").click()
+        self.driver.find_element(By.ID,"react-burger-menu-btn").click()
+        self.driver.find_element(By.ID,"logout_sidebar_link").click()
